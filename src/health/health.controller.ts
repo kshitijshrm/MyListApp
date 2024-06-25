18652d163e9f68b88dcfeecfdd56d0e247be9fa5
shubham4delhivery <shubham.laddha@delhivery.com>
2024-06-25 11:56:39 +0530

    Move redis config local to health module

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -7,7 +7,7 @@ import {
   HealthCheck,
   HealthCheckService,
 } from '@nestjs/terminus';
-import { Cluster } from 'ioredis';
+import { Cluster, ClusterOptions } from 'ioredis';
 import { join } from 'path';
 import {
   redisClusterHosts,
@@ -24,6 +24,40 @@ export class HealthController {
     private readonly health: HealthCheckService,
     private readonly redisIndicator: RedisHealthIndicator,
   ) {
+    const host = process.env['REDIS_CLUSTER_HOST'] as string;
+    const hostsString =
+      (process.env['REDIS_CLUSTER_MULTI_HOST'] as string) || host;
+    const password = process.env['REDIS_CLUSTER_PASSWORD'] as string;
+    const port = parseInt(process.env['REDIS_CLUSTER_PORT'] as string, 10);
+    const retryCount =
+      parseInt(process.env['REDIS_RETRIES'] as string, 10) || 3;
+    const namespace = process.env['f_redis_namespace'];
+
+    const redisClusterHosts = hostsString.split(',').map((host) => ({
+      host: host.trim(),
+      port: port,
+    }));
+
+    const redisClusterOptions: ClusterOptions = {
+      redisOptions: {
+        password: password,
+        maxRetriesPerRequest: retryCount,
+        tls:
+          process.env['f_stage'] != 'local'
+            ? {
+                servername: host,
+                minVersion: 'TLSv1.2',
+                rejectUnauthorized: false,
+              }
+            : undefined,
+      },
+      slotsRefreshTimeout: 10000,
+      slotsRefreshInterval: 50000,
+    };
+
+    if (namespace) {
+      redisClusterOptions.keyPrefix = namespace;
+    }
     this.redis = new Cluster(redisClusterHosts, redisClusterOptions);
   }
 
