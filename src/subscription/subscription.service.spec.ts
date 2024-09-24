d9f6bbd4b6053a45fc6490f26ddfd6aa8096eb79
shubham4delhivery <shubham.laddha@delhivery.com>
2024-09-24 13:34:09 +0530

    Fix unit tests

--- src/subscription/subscription.service.spec.ts
+++ src/subscription/subscription.service.spec.ts
@@ -1,7 +1,4 @@
-import {
-  SubscriptionDTO,
-  SubscriptionsResponseDTO,
-} from 'src/common/dto/subscription/subscription.dto';
+import { SubscriptionDTO } from 'src/common/dto/subscription/subscription.dto';
 import { TestHelpers } from 'src/common/test/test.helpers';
 import { CoreosAgentServiceClient } from 'src/shared/schemas/os1/core/service/coreosagent.pb';
 import { FileServiceClient } from 'src/shared/schemas/os1/core/service/file.pb';
@@ -313,11 +310,7 @@ describe('SubscriptionService', () => {
         .mockImplementation(() => of({ subscriptions: sampleSubscriptions }));
       jest.spyOn(redisService, 'get').mockImplementation(async () => undefined);
       const ctx = TestHelpersBase.CreatePlatformContext();
-      const result = await service.getActiveSubscriptions(
-        ctx,
-        'user-id',
-        'tenant-id',
-      );
+      const result = await service.getActiveSubscriptions(ctx, 'tenant-id');
       expect(
         subscriptionServiceClient.getSubscriptionsByTenantId,
       ).toHaveBeenCalledTimes(1);
@@ -332,11 +325,7 @@ describe('SubscriptionService', () => {
         .spyOn(redisService, 'get')
         .mockImplementation(async () => JSON.stringify(sampleSubscriptions));
       const ctx = TestHelpersBase.CreatePlatformContext();
-      const result = await service.getActiveSubscriptions(
-        ctx,
-        'user-id',
-        'tenant-id',
-      );
+      const result = await service.getActiveSubscriptions(ctx, 'tenant-id');
       expect(
         subscriptionServiceClient.getSubscriptionsByTenantId,
       ).toHaveBeenCalledTimes(2);
@@ -351,11 +340,7 @@ describe('SubscriptionService', () => {
         .spyOn(redisService, 'get')
         .mockImplementation(async () => JSON.stringify(sampleSubscriptions2));
       const ctx = TestHelpersBase.CreatePlatformContext();
-      const result = await service.getActiveSubscriptions(
-        ctx,
-        'user-id',
-        'tenant-id',
-      );
+      const result = await service.getActiveSubscriptions(ctx, 'tenant-id');
       expect(
         subscriptionServiceClient.getSubscriptionsByTenantId,
       ).toHaveBeenCalledTimes(3);
