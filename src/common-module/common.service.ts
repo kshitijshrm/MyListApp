b46c86af5b08bd5edf86c598eee0d8201b09eca4
shubham4delhivery <shubham.laddha@delhivery.com>
2024-10-05 16:00:16 +0530

    Introduce solutionId and activeOnly filters

--- src/common-module/common.service.ts
+++ src/common-module/common.service.ts
@@ -1,9 +1,16 @@
 import { PlatformRequestContext } from '@foxtrotplatform/developer-platform-core-lib';
 import { Injectable, Logger, Inject } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
+import { DateTime } from 'luxon';
 import { firstValueFrom } from 'rxjs';
-import { BannerResponseDTO } from 'src/common/dto/common/common.dto';
+import {
+  BannerAllResponseDTO,
+  BannerDTO,
+  BannerLatestResponseDTO,
+  BannerResponseDataDTO,
+} from 'src/common/dto/common/common.dto';
 import { TenantBannersResponseSchemaToDtoMapper } from 'src/common/dto/common/response.dto.mapper';
+import { bannerLevelTypeFromJSON } from 'src/shared/schemas/os1/core/coreosagent/request.pb';
 import {
   COREOS_AGENT_SERVICE_NAME,
   CoreosAgentServiceClient,
@@ -31,7 +38,11 @@ export class CommonService {
   async getApplicableBanners(
     ctx: PlatformRequestContext,
     tenantId: string,
-  ): Promise<BannerResponseDTO> {
+    params: {
+      solutionId?: string;
+      filterActiveOnly?: boolean;
+    },
+  ): Promise<BannerLatestResponseDTO | BannerAllResponseDTO> {
     const tenantSubscriptions =
       await this.subscriptionService.getActiveSubscriptions(ctx, tenantId);
     const tenantSubscriptionMap = tenantSubscriptions
@@ -51,13 +62,92 @@ export class CommonService {
         ctx.rpcMetadata,
       ),
     );
+    const applicableBanners =
+      TenantBannersResponseSchemaToDtoMapper.mapToBannerResponseDTO(
+        bannersResponse.banners || [],
+        tenantSubscriptionMap,
+      );
+    if (params.solutionId && params.filterActiveOnly) {
+      return {
+        serverTime: bannersResponse.serverTimeIso,
+        tenantTimeZone: bannersResponse.tenantTimezone,
+        banner: this.getActiveBanner(bannersResponse.serverTimeIso, [
+          ...applicableBanners.solution.filter(
+            (banner) => banner.subscription.solutionId === params.solutionId,
+          ),
+          ...applicableBanners.foundation,
+        ]),
+      };
+    } else if (params.solutionId) {
+      return {
+        serverTime: bannersResponse.serverTimeIso,
+        tenantTimeZone: bannersResponse.tenantTimezone,
+        banners: {
+          foundation: applicableBanners.foundation,
+          solution: applicableBanners.solution.filter(
+            (banner) => banner.subscription.solutionId === params.solutionId,
+          ),
+        },
+      };
+    } else if (params.filterActiveOnly) {
+      return {
+        serverTime: bannersResponse.serverTimeIso,
+        tenantTimeZone: bannersResponse.tenantTimezone,
+        banner: this.getActiveBanner(bannersResponse.serverTimeIso, [
+          ...applicableBanners.solution,
+          ...applicableBanners.foundation,
+        ]),
+      };
+    }
     return {
       serverTime: bannersResponse.serverTimeIso,
       tenantTimeZone: bannersResponse.tenantTimezone,
-      banners: TenantBannersResponseSchemaToDtoMapper.mapToBannerResponseDTO(
-        bannersResponse.banners || [],
-        tenantSubscriptionMap,
-      ),
+      banners: applicableBanners,
     };
+  }
+
+  private getActiveBanner(
+    serverTimeIso: string,
+    banners: BannerDTO[],
+  ): BannerDTO {
+    const activeBanners = banners.filter(
+      (banner) =>
+        DateTime.fromISO(banner.activeUntilTime).diff(
+          DateTime.fromISO(serverTimeIso),
+          'minutes',
+        ).minutes > 0 &&
+        DateTime.fromISO(serverTimeIso).diff(
+          DateTime.fromISO(banner.activeFromTime),
+          'minutes',
+        ).minutes > 0,
+    );
+    if (activeBanners.length) {
+      return activeBanners.sort((bannerA, bannerB) => {
+        const [durA, durB] = [
+          DateTime.fromISO(bannerA.activeUntilTime).diff(
+            DateTime.fromISO(bannerA.activeFromTime),
+            'minutes',
+          ).minutes,
+          DateTime.fromISO(bannerB.activeUntilTime).diff(
+            DateTime.fromISO(bannerB.activeFromTime),
+            'minutes',
+          ).minutes,
+        ];
+        if (durA === durB) {
+          return (
+            bannerLevelTypeFromJSON(bannerA.levelType) -
+            bannerLevelTypeFromJSON(bannerB.levelType)
+          );
+        }
+        return durB - durA;
+      })[0];
+    }
+
+    return banners.sort((bannerA, bannerB) => {
+      return DateTime.fromISO(bannerA.activeFromTime).diff(
+        DateTime.fromISO(bannerB.activeFromTime),
+        'minutes',
+      ).minutes;
+    })[0];
   }
 }
